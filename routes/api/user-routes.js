4a196f7b6db38d6c84ac5d2f0add4c44dfc60a03
DawnWogerman <99206850+DawnWogerman@users.noreply.github.com>
2022-06-25 20:58:16 -0600

    added authentication

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -48,24 +48,49 @@ router.post('/', (req, res) => {
         });
 });
 
+router.post('/login', (req,res) =>{
+  //query ops
+    User.findOne({
+      where: {
+        email: req.body.email
+      }
+    })
+      .then(dbUserData => {
+        if (!dbUserData) {
+          res.status(400).json({ message: 'No user with that email address!'});
+          return;
+        }
+      const validPassword = dbUserData.checkPassword(req.body.password);
+
+      if (!validPassword) {
+        res.status(400).json({ message: 'Incorrect password!' });
+        return;
+      }
+  
+      res.json({ user: dbUserData, message: 'You are now logged in!' });
+  
+      });
+});
+
 // PUT /api/users/1
 router.put('/:id', (req, res) => {
-    User.update(req.body, {
-        where: {
-          id: req.params.id
-        }
-      })
-        .then(dbUserData => {
-          if (!dbUserData[0]) {
-            res.status(404).json({ message: 'No user found with this id' });
-            return;
-          }
-          res.json(dbUserData);
-        })
-        .catch(err => {
-          console.log(err);
-          res.status(500).json(err);
-        });
+  User.update(req.body, {
+    individualHooks: true,
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbUserData => {
+      if (!dbUserData[0]) {
+        res.status(404).json({ message: 'No user found with this id' });
+        return;
+      }
+      res.json(dbUserData);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 // DELETE /api/users/1
