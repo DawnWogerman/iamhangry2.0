7449c5834c48a82ba3082f23ff4484e41e832abe
DawnWogerman <99206850+DawnWogerman@users.noreply.github.com>
2022-06-29 18:51:34 -0600

    fixing heroku deployement

--- config/connection.js
+++ config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 
 // create connection to our db
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost',
+  host: 'n4m3x5ti89xl6czh.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
   dialect: 'mysql',
   port: 3306
 });
